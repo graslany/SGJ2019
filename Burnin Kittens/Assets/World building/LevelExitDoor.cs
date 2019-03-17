b5a5ba41aac239d5456e35434a9d689b9a79f7e2
Yves Grasland <yves.grasland@gmx.fr>
2019-03-17 08:17:44 +0100

    Added a minimal end game mechanics

---
+++ Burnin Kittens/Assets/World building/LevelExitDoor.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExitDoor : MonoBehaviour, IInteractible {
+    public void AcceptBob(Bob source)
+    {
+        SceneManager.LoadScene("WonGame");
+    }
+}
