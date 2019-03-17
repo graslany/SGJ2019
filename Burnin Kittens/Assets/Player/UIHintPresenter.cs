c8442e81e6e8f4bb18bda1e99d13f74ca3f35e70
Yves Grasland <yves.grasland@gmx.fr>
2019-03-17 13:08:05 +0100

    Bug fixes

---
+++ Burnin Kittens/Assets/Player/UIHintPresenter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+public class UIHintPresenter : MonoBehaviour {
+    
+    public SpriteRenderer target;
+    public UIHint lastHint;
+
+    protected virtual void OnTriggerEnter2D(Collider2D other) {
+        var h = other.GetComponent<UIHint>();
+        lastHint = h ?? lastHint;
+    }
+
+    protected virtual void Update() {
+        if (lastHint != null && (lastHint.transform.position - transform.position).magnitude <= lastHint.maxDistance) {
+            target.sprite = lastHint.hint;
+        }
+        else {
+            target.sprite = null;
+        }
+    }
+}
